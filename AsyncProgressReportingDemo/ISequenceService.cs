186dc0e0337043524c0f303997c7f2f13c9584bf
Vlad Moglan <vlad.moglan@gmail.com>
2021-05-08 11:31:45 +0200

    Added comments.

--- AsyncProgressReportingDemo/ISequenceService.cs
+++ AsyncProgressReportingDemo/ISequenceService.cs
@@ -8,6 +8,11 @@ namespace AsyncProgressReportingDemo
 {
     public interface ISequenceService
     {
+        /// <summary>
+        /// Long-running CPU-bound operation made async to prevent blocking the UI.
+        /// </summary>
+        /// <param name="sequence">The sequence to execute.</param>
+        /// <param name="progress">Used for reporting the progress of each step (not of the sequence as a whole).</param>
         Task ExecuteAsync(IEnumerable<IStep> sequence, IProgress<StepProgressEventArgs> progress, CancellationToken token);
     }
 }
