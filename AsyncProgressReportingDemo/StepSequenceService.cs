b58e14ce8afc980c80a11dda6416fc00a31bdaa0
Vlad Moglan <vlad.moglan@gmail.com>
2021-05-08 11:11:06 +0200

    Ensuring event unsubscribing.

--- AsyncProgressReportingDemo/StepSequenceService.cs
+++ AsyncProgressReportingDemo/StepSequenceService.cs
@@ -20,8 +20,15 @@ namespace AsyncProgressReportingDemo
                 foreach (var step in steps)
                 {
                     step.ProgressChanged += c_ProgressChanged;
-                    step.Execute();
-                    step.ProgressChanged -= c_ProgressChanged;
+
+                    try
+                    {
+                        step.Execute();
+                    }
+                    finally
+                    {
+                        step.ProgressChanged -= c_ProgressChanged;
+                    }
                 }
             }, 
             token);
