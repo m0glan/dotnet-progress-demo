88da98e3d6258d4a65c67a56dd69d4f4cfc285ce
Vlad Moglan <vlad.moglan@gmail.com>
2021-05-08 02:59:32 +0200

    Added IStepSequenceService.

---
+++ AsyncProgressReportingDemo/IStepSequenceService.cs
@@ -0,0 +1,13 @@
+﻿using AsyncProgressReportingDemo.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncProgressReportingDemo
+{
+    public interface IStepSequenceService
+    {
+        Task Execute(IEnumerable<IStep> steps, IProgress<StepProgressEventArgs> progress, CancellationToken token);
+    }
+}
---
+++ AsyncProgressReportingDemo/StepSequenceService.cs
@@ -0,0 +1,30 @@
+﻿using AsyncProgressReportingDemo.Core;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncProgressReportingDemo
+{
+    public class StepSequenceService : IStepSequenceService
+    {
+        public async Task Execute(IEnumerable<IStep> steps, IProgress<StepProgressEventArgs> progress, CancellationToken token)
+        {
+            await Task.Run(() =>
+            {
+                void c_ProgressChanged(object sender, StepProgressEventArgs e)
+                {
+                    progress?.Report(e);
+                }
+
+                foreach (var step in steps)
+                {
+                    step.ProgressChanged += c_ProgressChanged;
+                    step.Execute();
+                    step.ProgressChanged -= c_ProgressChanged;
+                }
+            }, 
+            token);
+        }
+    }
+}
